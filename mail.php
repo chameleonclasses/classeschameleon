a9b957ab6e3e7cce0f7d7dd2096823040fa2bd73
techcreation <chameleonclasses@gmail.com>
2020-06-20 10:13:10 +0530

    form update

--- mail.php
+++ mail.php
@@ -1,27 +1,23 @@
 <?php
-    $to = 'demo@site.com';
+    $to = 'lalteshsharma1997@gmail.com';
     $name = $_POST["name"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $subject= $_POST["subject"];
-    
+    $qualifications = $_POST["qualifications "];
+    $specialization = $_POST["specialization"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+    $areapin = $_POST["areapin"];
+    $subject = $_POST["subject"];
+    $class = $_POST["class"];
+    $experience = $_POST["experience"];
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$name.'  '.$subject.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$subject.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-        
-    </table>';
+    
 
-    if (@mail($to, $email, $message, $headers))
+    if (@mail($to, $name, $qualifications, $specialization, $phone, $email, $areapin, $subject, $class, $experience, $headers))
     {
         echo 'Your message has been sent.';
     }else{
